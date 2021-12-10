1768288896b8e972702d9aac86b093086854dc6a
Cosdaman <Cosdaguy@gmail.com>
2021-12-09 19:04:37 -0800

    added get for categories

--- models/index.js
+++ models/index.js
@@ -6,15 +6,19 @@ const ProductTag = require('./ProductTag');
 const { DataTypes } = require('sequelize/dist');
 
 // Products belongsTo Category
-Product.belongsTo(Category)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 // Categories have many Products
 Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
   through: 'product_tag'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
   through: 'product_tag'
 })
 
