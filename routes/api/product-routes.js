98e1bf42b13f7d270826ff9fe0e1f799a861a015
Cosdaman <Cosdaguy@gmail.com>
2021-12-12 16:13:18 -0800

    delete products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -120,6 +120,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
